e52d43e0dd867cad0a13996c15fc99ecacff65f3
Thiago <thiago099carvalho@gmail.com>
2022-07-02 10:54:20 -0300

    feat: add registerDelivery method
    Co-authered-by: Denys Rogeres <dennysrogeres@gmail.com>

--- src/Entrega.hs
+++ src/Entrega.hs
@@ -2,5 +2,26 @@ module Entrega where
 
 import System.IO
 import Data.List.Split (splitOn)
+import System.Console.ANSI
+
+-- productExists :: [String] -> Bool ()
+--   productExists codigo= do
+--     arq <- openFile "products.txt" ReadMode
+--     return text =~ "Código " ++ codigo
+
 
 registerDelivery :: IO ()
+registerDelivery = do
+    arq <- openFile "delivery.txt" AppendMode
+    putStrLn "Digite o código da compra:"
+    codigo <- getLine
+      -- if productExists codigo
+      -- then do
+    putStrLn "Digite o endereço de entrega:"
+    endereco <- getLine
+    let entrega = "Endereço: " ++ endereco ++ ";" ++ "Código: " ++ codigo ++ ";" ++ "Status da Entrega: Em Progresso\n"
+    hPutStr arq entrega
+    hClose arq
+    putStrLn "Processo de Entrega cadastrada com sucesso!"
+      -- else
+      --   putStrLn "Produto não existe\n"
